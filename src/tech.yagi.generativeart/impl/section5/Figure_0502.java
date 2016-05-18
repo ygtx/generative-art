15dbeb44a203cfa8c5540a5159f8841b2fe75fcf
yagithub <jagigyajujamobile@gmail.com>
2016-05-18 09:50:58 +0900

    Figure 0502

--- src/tech.yagi.generativeart/impl/section5/Figure_0502.java
+++ src/tech.yagi.generativeart/impl/section5/Figure_0502.java
@@ -3,7 +3,7 @@ package tech.yagi.generativeart.impl.section5;
 import processing.core.PApplet;
 
 
-public class Figure_0501 extends PApplet {
+public class Figure_0502 extends PApplet {
 
     public void settings() {
         size(300, 300);
@@ -13,26 +13,21 @@ public class Figure_0501 extends PApplet {
         smooth();
         background(255);
 
-        noLoop();
-    }
 
+//        noLoop();
 
-    public void draw() {
-        background(255);
         float xstart = random(10);
         float xnoise = xstart;
         float ynoise = random(10);
 
-        for (int y = 0; y <= height; y += 1) {
+        for (int y = 0; y <= height; y += 5) {
 
-            ynoise += 0.01f;
+            ynoise += 0.1f;
             xnoise = xstart;
 
-            for (int x = 0; x <= width; x += 1) {
-                xnoise += 0.01;
-                int alpha = (int)(noise(xnoise, ynoise) * 255);
-                stroke(0, alpha);
-                line(x, y, x + 1, y + 1);
+            for (int x = 0; x <= width; x += 5) {
+                xnoise += 0.1;
+                drawPoint(x, y, noise(xnoise, ynoise));
             }
 
         }
@@ -40,6 +35,15 @@ public class Figure_0501 extends PApplet {
 
     }
 
+    void drawPoint(float x, float y, float noiseFactor) {
+//        strokeWeight(1.5f);
+        sysout(noiseFactor);
+//        float len = constrain(10 * noiseFactor, 0.0f, 4.0f);
+        float len = 10 * noiseFactor;
+//        float len = 8.5f;
+        sysout(len);
+        rect(x, y, len, len);
+    }
 
     public void mousePressed() {
         redraw();
